b84721d6e951e54f1cd9429c2128e18f9cb7bf84
Yookyubin <binde73@naver.com>
2024-03-08 21:43:34 +0900

    E13. random spawning
    camping play 방지
    enemy random spawn

--- Assets/Script/Spawner.cs
+++ Assets/Script/Spawner.cs
@@ -8,6 +8,9 @@ public class Spawner : MonoBehaviour
     public Wave[] waves;
     public Enemy enemy;
 
+    LivingEntity playerEntity;
+    Transform playerT;
+
     Wave currentWave;
     int currentWaveNumber = 0;
 
@@ -15,20 +18,82 @@ public class Spawner : MonoBehaviour
     int enemiesRemainingAlive;
     float nextSpawnTime;
 
+    MapGenerator map;
+
+    float timeBetweenCampingChecks = 2;
+    float campThresholdDistance = 1.5f;
+    float nextCampCheckTime;
+    Vector3 campPositionOld;
+    bool isCamping;
+
+    bool isDisabled;
+
     void Start()
     {
+        playerEntity = FindObjectOfType<Player>();
+        playerT = playerEntity.transform;
+
+        nextCampCheckTime = timeBetweenCampingChecks + Time.time;
+        campPositionOld = playerT.position;
+        playerEntity.OnDeath += OnPlayerDeath;
+
+        map = FindObjectOfType<MapGenerator>();
         NextWave();    
     }
     void Update()
     {
+        if (isDisabled) 
+            return;
+
+        if (Time.time > nextCampCheckTime)
+        {
+            nextCampCheckTime = Time.time + timeBetweenCampingChecks;
+
+            isCamping = (Vector3.Distance(playerT.position, campPositionOld) < campThresholdDistance);
+            campPositionOld = playerT.position;
+        }
+
         if (enemiesRemainingToSpawn > 0 && Time.time > nextSpawnTime)
         {
             enemiesRemainingToSpawn--;
             nextSpawnTime = Time.time + currentWave.timeBetweenSpawns;
 
-            Enemy spawnedEnemy = Instantiate(enemy, Vector3.zero, Quaternion.identity) as Enemy;
-            spawnedEnemy.OnDeath += OnEnemyDeath; // 이벤트 달아두기, 구독?
+            StartCoroutine(SpawnEnemy());
         }
+    }
+
+    IEnumerator SpawnEnemy()
+    {
+        float spawnDelay = 1;
+        float tileFlashSpeed = 4;
+
+        Transform spawnTile = map.GetRandomOpenTile();
+        if (isCamping)
+        {
+            spawnTile = map.GetTileFromPosition(playerT.position);
+        }
+        Material tileMat = spawnTile.GetComponent<Renderer>().material; // 이 시점에 tile 색상이 빨간색이였다면 계속 빨간색임. 색상 초기화 방법을 다시 생각해볼것
+        Color initialColor = tileMat.color;
+        Color flashColor = Color.red;
+        float spawnTimer = 0;
+
+        while (spawnTimer < spawnDelay)
+        {
+            tileMat.color = Color.Lerp(initialColor, flashColor, Mathf.PingPong(spawnTimer * tileFlashSpeed, 1));
+
+            spawnTimer += Time.deltaTime;
+            yield return null;
+        }
+
+        Enemy spawnedEnemy = Instantiate(enemy, spawnTile.position, Quaternion.identity) as Enemy;
+        spawnedEnemy.OnDeath += OnEnemyDeath; // 이벤트 달아두기, 구독?
+
+
+    }
+
+    void OnPlayerDeath()
+    {
+        isDisabled = true;
     }
 
     void OnEnemyDeath()
